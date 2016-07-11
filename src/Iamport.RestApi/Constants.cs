26cb17d8eef47dd70c2a13bf28b27a684d1a227c
Gongdo <gongdo@live.com>
2016-07-11 18:09:03 +0900

    #26 상수값을 모아두는 클래스 추가

---
+++ src/Iamport.RestApi/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Iamport.RestApi
+{
+    /// <summary>
+    /// 상수값을 모아두는 클래스입니다.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// 응답이 성공임을 나타내는 코드입니다.
+        /// </summary>
+        public const int ResponseSuccessCode = 0;
+    }
+}
